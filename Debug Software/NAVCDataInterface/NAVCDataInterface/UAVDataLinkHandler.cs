a41bd396adcd2768b22d7ffbc263055455b02d56
Philip Salmony <34550389+pms67@users.noreply.github.com>
2019-12-14 18:22:52 +0100

    NAVC Serial Debugger program (first version)
    - Implemented in C#. Reads packet-ed data sent from NAVC to debug serial output.
    - Decodes COBS messages successfully.
    - Need to implement data extraction and plotting.
    - Should decide on what data is sent via debug...

---
+++ Debug Software/NAVCDataInterface/NAVCDataInterface/UAVDataLinkHandler.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAVCDataInterface
+{
+    class UAVDataLinkHandler
+    {
+        private int rxBufSize;
+        private int rxBufIndex;
+        private byte[] rxBuf;
+
+        public int packetLength;
+        public byte[] packetBuf;
+
+        public int SEQUENCE;
+        public int IDA;
+        public int IDB;
+        public int PAYLOADLENGTH;
+        public byte[] PAYLOAD;
+        public bool CHECKSUMCORRECT;
+        public byte CHECKSUM;
+        public byte RXCHECKSUM;
+
+        public UAVDataLinkHandler(int rxBufSize)
+        {
+            /* Initialise variables */
+            this.rxBufSize = rxBufSize;
+                        
+            rxBuf = new byte[rxBufSize];
+            for (int n = 0; n < rxBufSize; n++)
+            {
+                rxBuf[n] = 0;
+            }
+
+            rxBufIndex = 0;
+
+            packetBuf = new byte[256];
+            packetLength = 0;
+        }
+
+        public bool Feed(byte[] buf, int numRx)
+        {
+            bool validPacketReceived = false;
+
+            for (int n = 0; n < numRx; n++)
+            {
+                if (buf[n] == 0)
+                {
+                    /* Decode COBS */
+                    DecodeCOBS();
+
+                    /* Extract packet parameters */
+                    SEQUENCE = packetBuf[0];
+                    IDA = packetBuf[1];
+                    IDB = packetBuf[2];
+                    PAYLOADLENGTH = packetBuf[3];
+                    PAYLOAD = new byte[PAYLOADLENGTH];
+
+                    /* Extract packet payload and compute checksum */
+                    CHECKSUM = 0;
+
+                    for (int i = 0; i < PAYLOADLENGTH; i++)
+                    {
+                        PAYLOAD[i] = packetBuf[4 + i];
+                        CHECKSUM ^= PAYLOAD[i];
+                    }
+
+                    /* Check if checksums match */
+                    RXCHECKSUM = rxBuf[4 + PAYLOADLENGTH + 1];
+                    if (CHECKSUM == RXCHECKSUM)
+                    {
+                        CHECKSUMCORRECT = true;
+                        validPacketReceived = true;
+                    }
+
+                    /* Reset RX buffer */
+                    for (int i = 0; i < rxBufIndex; i++)
+                    {
+                        rxBuf[i] = 0;
+                    }
+                    rxBufIndex = 0;
+
+                    /* Reset packet buffer */
+                    for (int i = 0; i < packetLength; i++)
+                    {
+                        packetBuf[i] = 0;
+                    }
+                } else
+                {
+                    rxBuf[rxBufIndex] = buf[n];
+                    rxBufIndex++;
+                }
+            }
+
+            return validPacketReceived;
+        }
+
+        private void DecodeCOBS()
+        {
+            packetLength = 0;
+            int nextZeroIndex = rxBuf[0]; /* First byte sets location of first zero */
+
+            for (int dataInIndex = 1; dataInIndex < rxBufIndex; dataInIndex++)
+            {
+                if (dataInIndex == nextZeroIndex)
+                {
+                    packetBuf[packetLength] = 0;
+                    nextZeroIndex = dataInIndex + rxBuf[dataInIndex];
+                } else
+                {
+                    packetBuf[packetLength] = rxBuf[dataInIndex];
+                }
+
+                packetLength++;
+            }
+        }
+
+    }
+}
---
+++ Debug Software/NAVCDataInterface/NAVCDataInterface/frmMain.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace NAVCDataInterface
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtRawOutput = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnRefreshPorts = new System.Windows.Forms.Button();
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 707);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(1008, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(177, 17);
+            this.statusLabel.Text = "Connect to NAVC via COM port.";
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage3);
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl.Location = new System.Drawing.Point(12, 27);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(984, 560);
+            this.tabControl.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.txtRawOutput);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(976, 534);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Log";
+            // 
+            // txtRawOutput
+            // 
+            this.txtRawOutput.BackColor = System.Drawing.Color.White;
+            this.txtRawOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRawOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRawOutput.Location = new System.Drawing.Point(6, 6);
+            this.txtRawOutput.MaxLength = 1000000;
+            this.txtRawOutput.Multiline = true;
+            this.txtRawOutput.Name = "txtRawOutput";
+            this.txtRawOutput.ReadOnly = true;
+            this.txtRawOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRawOutput.Size = new System.Drawing.Size(964, 522);
+            this.txtRawOutput.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(976, 534);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Navigation Data";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(976, 534);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Plots";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 593);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(696, 105);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Commands";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Controls.Add(this.btnRefreshPorts);
+            this.groupBox2.Controls.Add(this.cmbBaud);
+            this.groupBox2.Controls.Add(this.cmbPort);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(714, 593);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(278, 105);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serial Connection";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(73, 74);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(124, 21);
+            this.btnConnect.TabIndex = 11;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnRefreshPorts
+            // 
+            this.btnRefreshPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefreshPorts.Location = new System.Drawing.Point(203, 20);
+            this.btnRefreshPorts.Name = "btnRefreshPorts";
+            this.btnRefreshPorts.Size = new System.Drawing.Size(69, 21);
+            this.btnRefreshPorts.TabIndex = 10;
+            this.btnRefreshPorts.Text = "Refresh";
+            this.btnRefreshPorts.UseVisualStyleBackColor = true;
+            this.btnRefreshPorts.Click += new System.EventHandler(this.btnRefreshPorts_Click);
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBaud.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Items.AddRange(new object[] {
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "128000",
+            "256000"});
+            this.cmbBaud.Location = new System.Drawing.Point(73, 47);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(124, 21);
+            this.cmbBaud.TabIndex = 9;
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(73, 20);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(124, 21);
+            this.cmbPort.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Serial Port:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "[HADES] NAVC Data Interface";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox txtRawOutput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnRefreshPorts;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
